85964d30cf1e957f4797fb222fabd30de03da5ee
Yldemaro R. <yldemaro.1994@gmail.com>
2025-07-06 16:51:46 -0400

    fix: correct base path in Vite config for production environment

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import ReactDOM from "react-dom/client";
 
 const routes = createBrowserRouter(
   createRoutesFromElements(AppRouter()), 
-  { basename: import.meta.env.DEV ? './' : '/personal-portfolio/' }
+  { basename: import.meta.env.DEV ? './' : './personal-portfolio/' }
 );
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
