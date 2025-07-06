fce302e43600720a2d3a3dcfd3241497fb39da2c
Yldemaro R. <yldemaro.1994@gmail.com>
2025-07-06 16:50:00 -0400

    refactor: enhance route creation and Vite config for improved clarity and maintainability

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
     base: './',
+  }
+
+  if (command !== 'serve') {
+    config.base = './personal-portfolio/'
   }
 
   return config
