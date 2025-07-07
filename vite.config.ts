b229bf4af2556e562299c090e9d31f232b3f94e0
Yldemaro R. <yldemaro.1994@gmail.com>
2025-07-07 15:37:37 -0400

    feat: add SVG icons and integrate vite-plugin-svgr
    - Added email.svg, github.svg, and linkedin.svg assets to the project.
    - Updated package.json to include vite-plugin-svgr for SVG handling.
    - Configured vite.config.ts to use svgrPlugin for importing SVGs as React components.
    - Updated vite-env.d.ts to include types for vite-plugin-svgr.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import svgrPlugin from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig(() => {
   const config = {
-    plugins: [react()],
+    plugins: [react(), svgrPlugin({
+      include: '**/*.svg',
+      svgrOptions: {
+        exportType: 'default',
+      },
+    })],
     base: '/', // Default base URL
   }
 
