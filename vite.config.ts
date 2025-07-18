91ddcc9822e884b675f08d4442ff93f05d043823
Yldemaro R. <yldemaro.1994@gmail.com>
2025-07-18 16:41:17 -0400

    feat: improve path manage

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import svgrPlugin from 'vite-plugin-svgr'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig(() => {
@@ -11,6 +12,13 @@ export default defineConfig(() => {
         exportType: 'default',
       },
     })],
+    resolve: {
+      alias: {
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@assets': path.resolve(__dirname, 'src/assets'),
+        '@utils': path.resolve(__dirname, 'src/utils')
+      }
+    },
     base: '/', // Default base URL
   }
 
